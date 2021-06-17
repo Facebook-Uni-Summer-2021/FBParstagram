986e06a23032d668b75866602bba8e9d0ce84541
reycritt <reycrittenden@gmail.com>
2021-06-17 16:41:16 -0500

    Completed Instagram Episode 3 and placed keys in *hopefully* sercure file

--- app/src/main/java/com/example/fbparstagram/LoginActivity.java
+++ app/src/main/java/com/example/fbparstagram/LoginActivity.java
@@ -2,13 +2,18 @@ package com.example.fbparstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.fbparstagram.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
@@ -24,6 +29,13 @@ public class LoginActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        //If a current user is already signed in,
+        // bypass login
+        if (ParseUser.getCurrentUser() != null) {
+            goToMainActivity();
+        }
+
+        //Login user
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,7 +47,50 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Login user using Parse
+     * @param username User's name
+     * @param password User's password
+     */
     private void loginUser(String username, String password) {
         Log.i(TAG, "Login user " + username);
+        //Runs on background thread for speed and space
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                //Handles onFailure
+                if (e != null) {
+                    Log.e(TAG, "Login issue: " + e);
+                    Toast.makeText(LoginActivity.this,
+                            "Login failed, please use correct " +
+                                    "username and password",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(LoginActivity.this,
+                        "Login successful!",
+                        Toast.LENGTH_SHORT).show();
+                goToMainActivity();
+            }
+        });
+    }
+
+    /**
+     * Sends user to MainActivity
+     */
+    private void goToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this,
+                MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
+/*
+Some notes:
+I used MongoDB instead of the other choice (Back4App seems to have updated)
+“Session” tracks all “User” activity
+Create a class to introduce a new model (such as User presenting a user, and Post representing a post on Instagram)
+To connect models/classes to one another, use “Pointer” as type of column
+
+Questions:
+ */
---
+++ app/src/main/java/com/example/fbparstagram/ParseApp.java
@@ -0,0 +1,26 @@
+package com.example.fbparstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApp extends Application {
+    private static final String TAG = "ParseApp";
+    //Placed keys in secret place just in case
+    private static final String APPLICATION_ID = BuildConfig.APPLICATION_ID;
+    private static final String CLIENT_KEY = BuildConfig.CLIENT_KEY;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Parse/Back4App copy-paste from API ref
+        //SECURE THESE THINGS!!!
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(APPLICATION_ID)
+                .clientKey(CLIENT_KEY)
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
